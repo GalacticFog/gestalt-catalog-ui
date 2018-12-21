fa1b14e819b773fe9c81ac34fef2facdb9a4a73d
John Betancur <johnnyazee@gmail.com>
2018-12-20 21:03:47 -0500

    generic plugins

---
+++ config/plugins/resource.js
@@ -0,0 +1,72 @@
+const Plugin = require('../../lib/Plugin');
+const { readFile } = require('../../lib/util');
+const path = require('path');
+
+class Resource extends Plugin {
+  async handler() {
+    const { filePath, files, icon, readme } = this.context;
+    const swaggerMatch = new RegExp(/metadata.(json|yaml|yml)/gi);
+    const entry = files.filter(f => swaggerMatch.test(f));
+
+    if (entry.length) {
+      const metaPath = path.join(filePath, entry[0]);
+      const meta = readFile(metaPath);
+      const payloadType = entry[0].endsWith('.yaml') || entry[0].endsWith('.yml')
+        ? 'yaml'
+        : 'json';
+
+      return {
+        meta: {
+          name: meta.name,
+          version: meta.version,
+          description: meta.description,
+          icon,
+        },
+        readme: readme,
+        deployable: true,
+        payload: {
+          type: payloadType,
+          render: 'none',
+          // data: JSON.stringify(meta),
+        },
+      };
+    } else {
+      throw new Error(`swagger.(json|yaml|yml) file not found`);
+    }
+  }
+}
+
+const container = {
+  name: 'container',
+  type: 'Container',
+  version: '1.0.0',
+  handler: Resource,
+};
+
+const lambda = {
+  name: 'lambda',
+  type: 'Lambda',
+  version: '1.0.0',
+  handler: Resource,
+};
+
+const bundle = {
+  name: 'bundle',
+  type: 'Bundle',
+  version: '1.0.0',
+  handler: Resource,
+};
+
+const virtualMachine = {
+  name: 'virtualMachine',
+  type: 'Virtual Machine',
+  version: '1.0.0',
+  handler: Resource,
+};
+
+module.exports = {
+  container,
+  lambda,
+  bundle,
+  virtualMachine,
+};
