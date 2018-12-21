b1716e264fb4c7a7ee1c5b5f9bab317280220a19
John Betancur <johnnyazee@gmail.com>
2018-12-20 20:10:42 -0500

    refactor compiler

--- config/plugins/api.js
+++ config/plugins/api.js
@@ -1,33 +1,28 @@
 const Plugin = require('../../lib/Plugin');
-const { readFile, readmeToHTML } = require('../../lib/util');
+const { readFile } = require('../../lib/util');
 const path = require('path');
 
 class API extends Plugin {
   async handler() {
-    const { filePath, files } = this.context;
+    const { filePath, files, icon, readme } = this.context;
     const swaggerMatch = new RegExp(/swagger.(json|yaml|yml)/gi);
     const entry = files.filter(f => swaggerMatch.test(f));
 
     if (entry.length) {
       const metaPath = path.join(filePath, entry[0]);
-      const meta = await readFile(metaPath);
+      const meta = readFile(metaPath);
       const payloadType = entry[0].endsWith('.yaml') || entry[0].endsWith('.yml')
         ? 'yaml'
         : 'json';
-
-      // Compile README
-      const readme = await readmeToHTML(
-        path.join(filePath, 'README.md')
-      );
-
+      
       return {
         meta: {
           name: meta.info.title,
           version: meta.info.version,
           description: meta.info.description,
-          icon: meta.info['x-icon'],
+          icon,
         },
-        readme,
+        readme: readme,
         payload: {
           type: payloadType,
           render: 'swagger',  
